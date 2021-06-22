e9523be402a1d3be50198d6dee4b4c146f004dfb
Tom Walden <tomwalden@gmail.com>
2021-06-22 01:38:33 +0100

    Adding main game class and states

---
+++ countercoup/model/items/states.py
@@ -0,0 +1,26 @@
+class SelectAction:
+    pass
+
+
+class DecideToBlock:
+    pass
+
+
+class DecideToCounteract:
+    pass
+
+
+class SelectCardsToDiscard:
+    pass
+
+
+class GameFinished:
+    pass
+
+
+class DecideToBlockCounteract:
+    pass
+
+
+class SelectCardToLose:
+    pass
---
+++ countercoup/model/player.py
@@ -0,0 +1,9 @@
+class Player:
+    """
+    Class for each Coup player
+    """
+
+    def __init__(self):
+        self.coins = 3
+        self.cards = []
+        self.in_game = True
