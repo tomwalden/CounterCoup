c89387043ec671a8fdce372bfa7536766b952d63
Tom Walden <tomwalden@gmail.com>
2021-08-02 00:53:31 +0100

    Switch to GameInfoSet

--- countercoup/player/agents/countercoup.py
+++ countercoup/player/agents/countercoup.py
@@ -2,7 +2,7 @@ from countercoup.shared.net_group import NetworkGroup
 from countercoup.shared.tools import Tools
 from countercoup.shared.infoset import Infoset
 from countercoup.player.agent import Agent
-from countercoup.model.game import Game
+from countercoup.model.game import GameInfoSet
 from countercoup.model.hand import Hand
 
 
@@ -19,20 +19,20 @@ class CounterCoup(Agent):
         total = sum(strategy.values())
         return {x: strategy[x] / total for x in strategy}
 
-    def get_action_strategy(self, g: Game) -> dict:
+    def get_action_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.action.get_output(Infoset(g), Tools.get_actions(g)))
 
-    def get_block_strategy(self, g: Game) -> dict:
+    def get_block_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.block.get_output(Infoset(g)))
 
-    def get_counteract_strategy(self, g: Game) -> dict:
+    def get_counteract_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.counteract.get_output(Infoset(g)))
 
-    def get_block_counteract_strategy(self, g: Game) -> dict:
+    def get_block_counteract_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.block.get_output(Infoset(g)))
 
-    def get_lose_card_strategy(self, g: Game) -> dict:
+    def get_lose_card_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.lose.get_output(Infoset(g), Hand.get_singular_hands(g.get_curr_player().cards)))
 
-    def get_discard_strategy(self, g: Game) -> dict:
+    def get_discard_strategy(self, g: GameInfoSet) -> dict:
         return self.__norm(self.net_group.lose.get_output(Infoset(g), Hand.get_all_hands(g.get_curr_player().cards)))
