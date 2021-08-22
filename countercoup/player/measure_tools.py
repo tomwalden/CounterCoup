2e8884ddb6bb662df2bc15ed013ba443354da0c3
Tom Walden <tomwalden@gmail.com>
2021-08-22 17:41:06 +0100

    Adding new measurement tools to the player

---
+++ countercoup/player/measure_tools.py
@@ -0,0 +1,29 @@
+from statistics import stdev, mean
+from countercoup.player.coup_play import CoupPlay
+
+
+class MeasureTools:
+
+    @staticmethod
+    def measure(agents: [], chunk: int, count: int):
+
+        stats = [[] for _ in agents]
+
+        for _ in count:
+            cp = CoupPlay(agents)
+
+            for _ in chunk:
+                cp.run()
+
+            for n, x in enumerate(cp.tally):
+                stats[n].append(x)
+
+        results = []
+
+        for s in stats:
+            results.append((mean(s), stdev(s) / (count ** 0.5)))
+
+        return results
+
+
+
